5d79569281b51a1bade483272ad3e51bd8f11725
albertovillar04 <alberto.villar@outlook.com>
2016-09-21 20:48:04 -0500

    laboratorio2

---
+++ laboratorios/laboratorio2/conversor de monedas.py
@@ -0,0 +1,12 @@
+print("CONVESOR DE CENTAVOS.")
+centavos = float(input("Introduce centavos: "))
+dolares = centavos/100
+print("La cantidad introducida de " + str(centavos) + " es igual a " + str(dolares) + " dolares.")
+euros = centavos*0.8965
+print("La cantidad introducida de " + str(centavos) + " es igual a " + str(euros) + " euros.")
+mxn = centavos * 19.7843
+print("La cantidad introducida de " + str(centavos) + " es igual a " + str(mxn) + " pesos mexicanos.")
+yen = centavos * 101.5744
+print("La cantidad introducida de " + str(centavos) + " es igual a " + str(yen) + " yen.")
+libras = centavos * 0.7702
+print("La cantidad introducida de " + str(centavos) + " es igual a " + str(libras) + " libras.")
